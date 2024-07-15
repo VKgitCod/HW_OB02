3d80a2183cdba6994346ff4018ba93dd84e7ab1a
VKgitCod <itcreatorvak@gmail.com>
2024-07-15 22:36:35 +0300

    mod code

--- main.py
+++ main.py
@@ -1,34 +1,47 @@
 class User():
-    def __init__(self, id, name, status):
-        self.id = id
-        self.name = name
-        self.status = "user"
+    def __init__(self, id, name):
+        self._id = id
+        self._name = name
+        self._status = "user"
         self.users = []
 
     def info(self):
         print(f"Список пользователей {self.users}")
 
+    def user_id(self):
+        return self._id
+
+    def set_name(self, name):
+        self._name = name
+
 class Admin(User):
-    def __init__(self, id, name, status):
-        super().__init__(id, name, status)
+    def __init__(self, id, name):
+        super().__init__(id, name)
         self._status = "admin"
 
-    def _add_user(self, id, name):
+    def add_user(self, id, name):
+        self.name = name
         self.users.append({"id": id, "name": name, "Статаус": "Пользователь"})
         print(f"Пользователь {self.name} добавлен")
 
-    def _remove_user(self, name):
+    def remove_user(self, name):
         for u in self.users:
             if u["name"] == name:
-                self.users.remove(name)
+                self.users.remove(u)
+                print(f"Пользователь {name} удален")
             else:
                 print("Пользователь не найден")
 
-adm = Admin(1, "Виктор", "")
+adm = Admin(1, "Виктор")
 
-adm._add_user(2, "Максим")
+adm.add_user(2, "Максим")
+adm.add_user(3, "Алексей")
+adm.add_user(4, "Игорь")
+adm.add_user(5, "Иван")
+adm.add_user(6, "Антон")
 adm.info()
 
-adm._remove_user("Максим")
+adm.remove_user("Антон")
 adm.info()
+
 
